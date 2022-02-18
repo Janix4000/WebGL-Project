047feb10c58ad2223ece7cf2d835ba70974fd33d
Janix4000 <janix4000@gmail.com>
2022-02-18 23:42:04 +0100

    import different modules

--- src/main.js
+++ src/main.js
@@ -1,255 +1,149 @@
+// import * as THREE from 'three';
 
-let scene;
+// import { GUI } from './jsm/libs/lil-gui.module.min.js';
 
+// import { OrbitControls } from './jsm/controls/OrbitControls.js';
 
-let camera;
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import { GUI } from 'https://cdn.skypack.dev/lil-gui';
 
-let christmasTree = [];
-let car = [];
-
-
-initializeScene();
-
-
-animateScene();
-
-/**
- * Initialize the scene.
- */
-function initializeScene() {
-
-    renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
+let camera, scene, renderer;
 
 
-    renderer.setClearColor(0x000000, 1);
 
 
-    canvasWidth = window.innerWidth;
-    canvasHeight = window.innerHeight;
 
-    renderer.setSize(canvasWidth, canvasHeight);
+const params = {
+    clipIntersection: true,
+    planeConstant: 0,
+    showHelpers: false
+};
 
-    document.getElementById("WebGLCanvas").appendChild(renderer.domElement);
+const clipPlanes = [
+    new THREE.Plane(new THREE.Vector3(1, 0, 0), 0),
+    new THREE.Plane(new THREE.Vector3(0, - 1, 0), 0),
+    new THREE.Plane(new THREE.Vector3(0, 0, - 1), 0)
+];
+
+init();
+render();
+
+function init() {
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.localClippingEnabled = true;
+    document.body.appendChild(renderer.domElement);
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 100);
-    camera.position.set(0, 2, 10);
-    camera.lookAt(scene.position);
-    scene.add(camera);
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 200);
 
-    let triangleMesh; {
-        const a = new THREE.Vector3(0.0, 1.0, 0.0);
-        const b = new THREE.Vector3(-1.0, -1.0, 0.0);
-        const c = new THREE.Vector3(1.0, -1.0, 0.0);
-        triangleMesh = createTriangle(a, b, c, 0xAAAAAA);
+    camera.position.set(- 1.5, 2.5, 3.0);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render); // use only if there is no animation loop
+    controls.minDistance = 1;
+    controls.maxDistance = 10;
+    controls.enablePan = false;
+
+    const light = new THREE.HemisphereLight(0xffffff, 0x080808, 1.5);
+    light.position.set(- 1.25, 1, 1.25);
+    scene.add(light);
+
+    // const helper = new THREE.CameraHelper( light.shadow.camera );
+    // scene.add( helper );
+
+    //
+
+    const group = new THREE.Group();
+
+    for (let i = 1; i <= 30; i += 2) {
+
+        const geometry = new THREE.SphereGeometry(i / 30, 48, 24);
+
+        const material = new THREE.MeshLambertMaterial({
+
+            color: new THREE.Color().setHSL(Math.random(), 0.5, 0.5),
+            side: THREE.DoubleSide,
+            clippingPlanes: clipPlanes,
+            clipIntersection: params.clipIntersection
+
+        });
+
+        group.add(new THREE.Mesh(geometry, material));
+
     }
-    triangleMesh.position.set(-1.5, 0.0, 4.0);
-    scene.add(triangleMesh);
 
-    let squareGeometry = new THREE.BufferGeometry();
-    const points = [
-        new THREE.Vector3(-1.0, 1.0, 0.0),
-        new THREE.Vector3(1.0, 1.0, 0.0),
-        new THREE.Vector3(1.0, -1.0, 0.0),
+    scene.add(group);
 
-        new THREE.Vector3(1.0, -1.0, 0.0),
-        new THREE.Vector3(-1.0, -1.0, 0.0),
-        new THREE.Vector3(-1.0, 1.0, 0.0),
-    ];
-    // squareGeometry.faces.push(new THREE.Face3(0, 1, 2));
-    // squareGeometry.faces.push(new THREE.Face3(0, 2, 3));
+    // helpers
 
-    const RECT_COLOR = 0xFF0000;
+    const helpers = new THREE.Group();
+    helpers.add(new THREE.PlaneHelper(clipPlanes[0], 2, 0xff0000));
+    helpers.add(new THREE.PlaneHelper(clipPlanes[1], 2, 0x00ff00));
+    helpers.add(new THREE.PlaneHelper(clipPlanes[2], 2, 0x0000ff));
+    helpers.visible = false;
+    scene.add(helpers);
 
-    let squareMaterial = new THREE.MeshBasicMaterial({
-        color: RECT_COLOR,
-        side: THREE.DoubleSide
+    // gui
+
+    const gui = new GUI();
+
+    gui.add(params, 'clipIntersection').name('clip intersection').onChange(function (value) {
+
+        const children = group.children;
+
+        for (let i = 0; i < children.length; i++) {
+
+            children[i].material.clipIntersection = value;
+
+        }
+
+        render();
+
     });
 
-    let squareMesh;
-    {
-        const a = new THREE.Vector3(-1.0, 1.0, 0.0);
-        const b = new THREE.Vector3(1.0, 1.0, 0.0);
-        const c = new THREE.Vector3(1.0, -1.0, 0.0);
-        const d = new THREE.Vector3(-1.0, -1.0, 0.0);
-        squareMesh = createRectangle(a, b, c, d, 0xFF0000);
-    }
-    squareMesh.position.set(1.0, 1.0, -10.0);
-    scene.add(squareMesh);
+    gui.add(params, 'planeConstant', - 1, 1).step(0.01).name('plane constant').onChange(function (value) {
 
-    let squareMesh2;
-    {
-        const a = new THREE.Vector3(-1.0, 1.0, 0.0);
-        const b = new THREE.Vector3(1.0, 1.0, 0.0);
-        const c = new THREE.Vector3(1.0, -1.0, 0.0);
-        const d = new THREE.Vector3(-1.0, -1.0, 0.0);
-        squareMesh2 = createRectangle(a, b, c, d, 0xFFFF00);
-    }
-    squareMesh2.position.set(3.0, 2.0, -8.0);
-    scene.add(squareMesh2);
+        for (let j = 0; j < clipPlanes.length; j++) {
 
-    addHome(scene);
+            clipPlanes[j].constant = value;
 
-    car = addCar(scene, new THREE.Vector3(-3, 2, 3));
+        }
 
-    christmasTree = addChristmasTree(scene, new THREE.Vector3(5, -3, 0));
+        render();
+
+    });
+
+    gui.add(params, 'showHelpers').name('show helpers').onChange(function (value) {
+
+        helpers.visible = value;
+
+        render();
+
+    });
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+
 }
 
-function animateScene() {
+function onWindowResize() {
 
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    for (let i = 0; i < christmasTree.length; i++) {
-        tree = christmasTree[i];
-        tree.rotation.y += 0.04;
-    }
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    for (let i = 0; i < car.length; i++) {
-        segment = car[i];
-        segment.position.x += 0.01;
-    }
-    for (let i = 1; i < car.length; i++) {
-        wheel = car[i];
-        wheel.rotation.z -= 0.01;
-    }
+    render();
 
-
-
-
-    requestAnimationFrame(animateScene);
-
-
-    renderScene();
 }
 
-/**
- * Render the scene. Map the 3D world to the 2D screen.
- */
-function renderScene() {
+function render() {
+
     renderer.render(scene, camera);
 
 }
-
-function createTriangle(a, b, c, color) {
-    const points = [
-        a, b, c
-    ];
-    const triangleGeometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    // triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
-
-    let triangleMaterial = new THREE.MeshBasicMaterial({
-        color: color,
-        side: THREE.DoubleSide
-    });
-
-    return new THREE.Mesh(triangleGeometry, triangleMaterial);
-}
-
-function createRectangle(a, b, c, d, color) {
-    const points = [
-        a, b, c, d
-    ];
-    let squareGeometry = new THREE.BufferGeometry().setFromPoints(points);
-
-
-    // squareGeometry.faces.push(new THREE.Face3(0, 1, 2));
-    // squareGeometry.faces.push(new THREE.Face3(0, 2, 3));
-
-
-
-
-    let squareMaterial = new THREE.MeshBasicMaterial({
-        color: color,
-        side: THREE.DoubleSide
-    });
-
-
-
-    return new THREE.Mesh(squareGeometry, squareMaterial);
-}
-
-function createCircle(radius, color) {
-
-    let geometry = new THREE.CircleGeometry(radius, Math.ceil(radius * 50));
-    let squareMaterial = new THREE.MeshBasicMaterial({
-        color: color,
-        side: THREE.DoubleSide
-    });
-
-
-
-    return new THREE.Mesh(geometry, squareMaterial);
-
-}
-
-function addHome(scene) {
-    let mesh = createRectangle(new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 0, 0), new THREE.Vector3(1, 1, 0), new THREE.Vector3(0, 1, 0), '#0000FF');
-    mesh.position.set(3.0, 2.0, 1);
-    scene.add(mesh);
-
-    mesh = createRectangle(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0.25, 0, 0), new THREE.Vector3(0.25, 0.25, 0), new THREE.Vector3(0, 0.25, 0), '#00FFFF');
-    mesh.position.set(3.1, 2.5, 1.1);
-    scene.add(mesh);
-
-    mesh = createRectangle(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0.25, 0, 0), new THREE.Vector3(0.25, 0.25, 0), new THREE.Vector3(0, 0.25, 0), '#00FFFF');
-    mesh.position.set(3.6, 2.5, 1.1);
-    scene.add(mesh);
-
-    mesh = createTriangle(new THREE.Vector3(0, 1, 0), new THREE.Vector3(1, 1, 0), new THREE.Vector3(0.5, 1.41, 0), '#FF00FF');
-    mesh.position.set(3.0, 2.0, 1);
-    scene.add(mesh);
-}
-
-function addChristmasTree(scene, position) {
-    let meshes = [];
-    let height = 0;
-    let scale = 1;
-    for (let i = 0; i < 6; i++) {
-        let mesh = createTriangle(new THREE.Vector3(-scale, height, 0), new THREE.Vector3(scale, height, 0), new THREE.Vector3(0, height + scale, 0), '#00BB00');
-        mesh.position.set(position.x, position.y, position.z)
-        scene.add(mesh);
-        height += scale;
-        scale -= 1 / (i + 4);
-        meshes.push(mesh);
-    }
-    return meshes;
-}
-
-function addCar(scene, position) {
-    let meshes = [];
-
-    let mesh = createRectangle(new THREE.Vector3(0, 0, 0), new THREE.Vector3(2, 0, 0), new THREE.Vector3(2, 1, 0), new THREE.Vector3(0, 1, 0), '#0000FF');
-    mesh.position.set(position.x, position.y, position.z);
-    scene.add(mesh);
-    meshes.push(mesh);
-
-    mesh = createCircle(0.3, '#00FFFF');
-    mesh.position.set(position.x + 0.1, position.y + 0.1, position.z + 0.1);
-    scene.add(mesh);
-    meshes.push(mesh);
-
-    mesh = createCircle(0.3, '#00FFFF');
-    mesh.position.set(position.x + 1.8, position.y + 0.1, position.z + 0.1);
-    scene.add(mesh);
-    meshes.push(mesh);
-
-    return meshes;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
